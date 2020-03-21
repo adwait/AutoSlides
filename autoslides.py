c094bd3998579f958c8cbb794fa58f0bbb53c272
adwait <godbole15@gmail.com>
2020-03-21 13:42:34 +0530

    merger completed

--- autoslides.py
+++ autoslides.py
@@ -13,7 +13,8 @@ import time
 import sys
 import os
 import shutil
-import exceptions
+import getopt
+# import exceptions
 
 import core.merger
 import core.utils
@@ -41,64 +42,69 @@ class autoslides:
     # modules = []
     currmodule = None
 
-    def setconfig(config):
-        # TODO: decide on configuration options and format and implement this method
-        print('custom config is currently disabled, sorry\n')
-        sys.exit(1)
+def setconfig(config):
+    # TODO: decide on configuration options and format and implement this method
+    print('custom config is currently disabled, sorry\n')
+    sys.exit(1)
 
-    def main():
+def main():
 
-        autoslides.cmdline = sys.argv
-        autoslides.timestamp = time.time()
+    autoslides.cmdline = sys.argv
+    autoslides.timestamp = time.time()
 
-        os.makedirs(os.path.dirname(autoslides.debugpath), exist_ok=True)
+    # os.makedirs(os.path.dirname(autoslides.debugpath), exist_ok=True)
 
-        print('')
-        print(' AutoSlides ')
-        print('------------')
-        print('')
+    print('')
+    print(' AutoSlides ')
+    print('------------')
+    print('')
 
-        # for module_name in modulelist:
-        #     try:
-        #         mod = importlib.import_module(module_dir + module_name)
-        #         autoslides.modules.append(getattr(mod, module_name)())
-        #     except ImportError as e:
-        #         print("import error for module {0}\nplease check the installation".format(module_name)
-        #         sys.exit(1)
-        #     except AttributeError as e:
-        #         print("attribute error for module {0}\nplease check the installation".format(module_name))
-        #     except Exception as e:
-        #         print("initialization failed for module {0}\nplease check the installation".format(module_name))
+    # for module_name in modulelist:
+    #     try:
+    #         mod = importlib.import_module(module_dir + module_name)
+    #         autoslides.modules.append(getattr(mod, module_name)())
+    #     except ImportError as e:
+    #         print("import error for module {0}\nplease check the installation".format(module_name)
+    #         sys.exit(1)
+    #     except AttributeError as e:
+    #         print("attribute error for module {0}\nplease check the installation".format(module_name))
+    #     except Exception as e:
+    #         print("initialization failed for module {0}\nplease check the installation".format(module_name))
 
-        try:
-            args = "hi:c:vd"
-            autoslides.options, autoslides.args = getopt.getopt(sys.argv[autoslides.cmdline[1:]])
+    try:
+        args = "hi:c:vd"
+        autoslides.options, autoslides.args = getopt.getopt(autoslides.cmdline[1:], args)
+    except getopt.GetoptError:
+        print('AutoSlides: incorrect usage\n')
 
-        except getopt.GetoptError, e:
-            print('AutoSlides: incorrect usage\n' + str(e))
+    for o, a in autoslides.options:
+        if o in "-v":
+            print(VERSION_NUMBER)
+        elif o in "-h":
+            print(help_string + '\n')
+        elif o in "-i":
+            autoslides.inputfile = a
+        elif o in "-c": 
+            autoslides.setconfig(a)
+        elif  o in "d":
+            autoslides.debug = True
 
-        for o, a in autoslides.options:
-            if o = "-v":
-                print VERSION_NUMBER
-            elif o in "-h":
-                print(help_string + '\n')
-            elif o in "-i":
-                autoslides.inputfile = a
-            elif o in "-c": 
-                autoslides.setconfig(a)
-            elif  o in "d":
-                autoslides.debug = True
+    # Merge LaTeX source into a single file
+    autoslides.currmodule = 'merger'
+    Merger = core.merger.Merger()
+    # print(autoslides.inputfile)
+    Merger.loadfromstring(autoslides.inputfile, autoslides)
+    output = Merger.getoutput()
 
-        # Merge LaTeX source into a single file
-        autoslides.currmodule = 'merger'
-        Merger = core.merger.Merger()
-        Merger.loadfromstring(input, autoslides)
-        output = Merger.getoutput()
+    print(output)
 
-        if autoslides.debug:
-            core.utils.savelog('{0}/merger__input.log'.format(autoslides.debugpath),input)
-            core.utils.savelog('{0}/merger__output.log'.format(autoslides.debugpath),output)
-            
+    if autoslides.debug:
+        core.utils.savelog('{0}/merger__input.log'.format(autoslides.debugpath),input)
+        core.utils.savelog('{0}/merger__output.log'.format(autoslides.debugpath),output)
         
+
+
+if __name__ == "__main__":
+    main()        
 
 
--- core/merger.py
+++ core/merger.py
@@ -1,3 +1,63 @@
 # TODO: add methods for
 #       merger to single file by reolving 'input'
 #       generate content tree for non-text content
+
+
+import sys
+import re
+
+class Merger:
+
+    mainpath = None
+    mainfiletext = None
+    
+    def __init__(self):
+        pass
+
+    def loadfromstring(self, input, asl):
+        self.mainpath = input
+        mainfile = open(input)
+        mainfiletext = mainfile.read()
+        
+    def getoutput(self):
+        maindirpath = '/'.join(self.mainpath.split('/')[:-1])
+        mainfilename = self.mainpath.split('/')[-1]
+        return self.getlinkedfiles(maindirpath, mainfilename)
+
+    def getlinkedfiles(self, dirpath, filename):
+        
+        # print(dirpath+filename)
+
+        def cleaninput(file):
+            file = file[7:-1].strip()
+            if file.endswith('.tex'):
+                pass
+            else:
+                file = file + '.tex'
+            return file
+        
+        def cleaninclude(file):
+            file = file[9:-1].strip()
+            if file.endswith('.tex'):
+                pass
+            else:
+                file = file + '.tex'
+            return file
+        
+
+        relpath = dirpath + '/' + filename
+
+        newdirpath = '/'.join(relpath.split('/')[:-1])
+        newfilename = relpath.split('/')[-1]
+        
+        f = open(newdirpath + '/' + newfilename)
+        ftext = f.read()
+
+        inputreg = re.compile(r'\\input{[^{}]*}')
+        includereg = re.compile(r'\\include{[^{}]*}')
+        
+
+        ftext = re.sub(inputreg, lambda x: self.getlinkedfiles(newdirpath, cleaninput(x.group())), ftext)
+        ftext = re.sub(includereg, lambda x: self.getlinkedfiles(newdirpath, cleaninclude(x.group())), ftext)
+
+        return ftext
